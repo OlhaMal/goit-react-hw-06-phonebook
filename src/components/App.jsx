0eb415a774c3b0c92bbdf0ea961c7619a2a7f15b
OlhaMal <117120298+OlhaMal@users.noreply.github.com>
2023-03-13 15:10:45 +0200

    refactoring with redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,35 @@
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-import { nanoid } from 'nanoid';
+import { getContacts, getFilter } from 'redux/selectors';
+import { useDispatch } from 'react-redux';
+import { addContact, deleteContact } from 'redux/actions';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem('contactList')) ?? [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
-  );
-  const [filter, setFilter] = useState('');
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const dispatch = useDispatch();
+
+  const [first, setFirst] = useState(true);
 
   useEffect(() => {
-    window.localStorage.setItem('contactList', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const handleChange = evt => {
-    const { value } = evt.target;
-    setFilter(value);
-  };
+    if (first) {
+      JSON.parse(window.localStorage.getItem('contactList'));
+      setFirst(false);
+    } else {
+      window.localStorage.setItem('contactList', JSON.stringify(contacts));
+    }
+  }, [contacts, first]);
 
   const handleSubmit = evt => {
-    const id = nanoid();
     const name = evt.name;
     const number = evt.number;
     const contactsList = [...contacts];
-
     if (contactsList.findIndex(contact => name === contact.name) !== -1) {
       toast.warn(`${name} is already in contacts.`, {
         position: 'top-right',
@@ -44,22 +42,20 @@ export const App = () => {
         theme: 'light',
       });
     } else {
-      contactsList.push({ id, name, number });
+      dispatch(addContact(name, number));
     }
-
-    setContacts(contactsList);
   };
 
   const handleDelete = evt => {
-    setContacts(contacts.filter(contact => contact.id !== evt))
-  }
+    dispatch(deleteContact(evt));
+  };
 
   const getFilteredContacts = () => {
     const filterContactsList = contacts.filter(contact => {
-      return contact.name.toLowerCase().includes(filter.toLowerCase())
-    })
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    });
     return filterContactsList;
-  }
+  };
 
   return (
     <div>
@@ -82,7 +78,7 @@ export const App = () => {
       >
         Contacts
       </h2>
-      <Filter filter={filter} handleChange={handleChange} />
+      <Filter />
       <ContactList
         contacts={getFilteredContacts()}
         handleDelete={handleDelete}
@@ -102,124 +98,3 @@ export const App = () => {
     </div>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//     name: '',
-//     number: '',
-//   };
-
-//   componentDidMount() {
-//     const localStorageSavedContacts = localStorage.getItem('contactList');
-//     const parsedContacts = JSON.parse(localStorageSavedContacts);
-
-//     if (parsedContacts) {
-//       this.setState({ contacts: parsedContacts });
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const prevStateContacts = prevState.contacts;
-//     const nexStateContacts = this.state.contacts;
-
-//     if (prevStateContacts !== nexStateContacts) {
-//       localStorage.setItem('contactList', JSON.stringify(nexStateContacts));
-//     }
-//   }
-
-//   handleChange = evt => {
-//     const { name, value } = evt.target;
-//     this.setState({ [name]: value });
-//   };
-
-//   handleSubmit = evt => {
-//     const id = nanoid();
-//     const name = evt.name;
-//     const number = evt.number;
-//     const contactsList = [...this.state.contacts];
-
-//     if (contactsList.findIndex(contact => name === contact.name) !== -1) {
-//       toast.warn(`${name} is already in contacts.`, {
-//         position: 'top-right',
-//         autoClose: 2000,
-//         hideProgressBar: false,
-//         closeOnClick: true,
-//         pauseOnHover: false,
-//         draggable: true,
-//         progress: undefined,
-//         theme: 'light',
-//       });
-//     } else {
-//       contactsList.push({ id, name, number });
-//     }
-//     this.setState({ contacts: contactsList });
-//   };
-
-//   handleDelete = evt => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== evt),
-//     }));
-//   };
-
-//   getFilteredContacts = () => {
-//     const filterContactsList = this.state.contacts.filter(contact => {
-//       return contact.name
-//         .toLowerCase()
-//         .includes(this.state.filter.toLowerCase());
-//     });
-
-//     return filterContactsList;
-//   };
-
-//   render() {
-//     const { filter } = this.state;
-
-//     return (
-//       <div>
-//         <h1
-//           style={{
-//             fontSize: 'xx-large',
-//             textAlign: 'center',
-//             marginTop: '90px',
-//           }}
-//         >
-//           Phonebook
-//         </h1>
-//         <ContactForm handleSubmit={this.handleSubmit} />
-//         <h2
-//           style={{
-//             textAlign: 'center',
-//             fontSize: 'x-large',
-//             marginTop: '5px',
-//           }}
-//         >
-//           Contacts
-//         </h2>
-//         <Filter filter={filter} handleChange={this.handleChange} />
-//         <ContactList
-//           contacts={this.getFilteredContacts()}
-//           handleDelete={this.handleDelete}
-//         />
-//         <ToastContainer
-//           position="top-right"
-//           autoClose={2000}
-//           hideProgressBar={false}
-//           newestOnTop={false}
-//           closeOnClick
-//           rtl={false}
-//           pauseOnFocusLoss
-//           draggable
-//           pauseOnHover={false}
-//           theme="light"
-//         />
-//       </div>
-//     );
-//   }
-// }
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,20 @@
-import PropTypes from 'prop-types';
 import css from './Filter.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getFilter } from 'redux/selectors';
+import { setFilter } from 'redux/actions';
 
-export const Filter = ({ filter, handleChange }) => (
-  <div className={css.filter}>
-    <label className={css.filterLabel}>Find contacts by name</label>
-    <input type="text" name="filter" value={filter} onChange={handleChange} />
-  </div>
-);
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
 
-Filter.propTypes = {
-  filter: PropTypes.string.isRequired,
-  handleChange: PropTypes.func.isRequired,
+  const handleChange = evt => {
+    dispatch(setFilter(evt.target.value));
+  };
+
+  return (
+    <div className={css.filter}>
+      <label className={css.filterLabel}>Find contacts by name</label>
+      <input type="text" name="filter" value={filter} onChange={handleChange} />
+    </div>
+  );
 };
